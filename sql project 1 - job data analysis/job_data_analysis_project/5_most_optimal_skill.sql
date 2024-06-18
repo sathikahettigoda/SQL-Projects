b5b9455f9e8b5facb6fbe7fcfe5f88b9d5cea8d7
Sathika Hettigoda <svhettigoda@outlook.com>
2024-06-18 19:25:15 +0530

    Project completed

--- sql project 1 - job data analysis/job_data_analysis_project/5_most_optimal_skill.sql
+++ sql project 1 - job data analysis/job_data_analysis_project/5_most_optimal_skill.sql
@@ -4,55 +4,31 @@ Question: What are the most optimal skills to learn (aka it's in high demand and
 - Concentrates on remote positions with specified salaries
 Why? Target skills that offer job security (high demand) and financial benefits (high salaries),
     offering strategic insights for career development in data analysis.
-*/
 
-WITH skills_demand AS (
-    SELECT
-        skills_dim.skill_id, 
-        skills_dim.skills,
-        COUNT(skills_job_dim.job_id) AS demand_count
-    FROM    
-        job_postings_fact
-    INNER JOIN 
-        skills_job_dim ON skills_job_dim.job_id = job_postings_fact.job_id
-    INNER JOIN 
-        skills_dim on skills_dim.skill_id = skills_job_dim.skill_id
-    WHERE 
-        job_postings_fact.job_title_short = 'Data Analyst' AND
-        salary_year_avg IS NOT NULL
-    GROUP BY
-        skills_dim.skill_id
-    ), avg_salary AS (
-    SELECT
-    skills_job_dim.skill_id,
-    ROUND(AVG(salary_year_avg),0) AS avg_salary
-FROM    
-    job_postings_fact
-INNER JOIN 
-    skills_job_dim ON skills_job_dim.job_id = job_postings_fact.job_id
-INNER JOIN 
-    skills_dim on skills_dim.skill_id = skills_job_dim.skill_id
-WHERE 
-    job_postings_fact.job_title_short = 'Data Analyst' AND
-    salary_year_avg IS NOT NULL AND
-    job_work_from_home = True
-GROUP BY
-    skills_job_dim.skill_id
-)
+    Combining insights from demand and salary data, this query aimed to pinpoint skills that are both in high demand and have high salaries, offering a strategic focus for skill development.
+*/
 
 
 SELECT
-    skills_demand.skill_id,
-    skills_demand.skills,
-    demand_count,
-    avg_salary
-FROM 
-    skills_demand
-INNER JOIN avg_salary ON 
-    avg_salary.skill_id = skills_demand.skill_id
+    skills_dim.skill_id, 
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(job_postings_fact.salary_year_avg),0) AS avg_salary
+FROM    
+    job_postings_fact
+INNER JOIN 
+    skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN 
+    skills_dim on skills_job_dim.skill_id = skills_dim.skill_id 
 WHERE 
-    demand_count > 10
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+GROUP BY
+    skills_dim.skill_id
+HAVING
+    COUNT(skills_job_dim.job_id) > 10
 ORDER BY
-    avg_salary DESC,
-    demand_count DESC    
-LIMIT 25
+    avg_salary DESC
+    -- demand_count DESC
+LIMIT 25;
